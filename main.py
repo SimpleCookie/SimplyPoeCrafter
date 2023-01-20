a67903e4520290d3393c6ced017698f82792d9e8
Marcus <Marcus.Ekstrom@devgroup.se>
2023-01-20 20:03:38 +0100

    Init with scope

---
+++ main.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+def setup_main_window(geo):
+  mainWindow = Tk()
+  mainWindow.title("Simply Crafter")
+  mainWindow.geometry(geo)
+  favicon = PhotoImage(file='favicon.png')
+  mainWindow.iconphoto(True, favicon)
+  mainWindow.config(background="#DDD")
+  return mainWindow
+
+def save_alt(pos):
+  global altLoc
+  print("pos" + str(pos))
+  altLoc = "3"
+
+def save_aug(pos):
+  global augLoc
+  print("pos" + str(pos))
+  augLoc = "4"
+
+def scope(save):
+  diameter = 60
+  prmpt = Tk()
+  prmpt.geometry("300x150")
+  prmpt.wm_attributes("-transparentcolor", "#60b26c")
+  frame = Frame(prmpt, width=diameter, height=diameter, bg="#60b26c")
+
+  myCanvas = Canvas(frame, width=diameter, height=diameter, bg="#60b26c")
+  myCanvas.create_line(0, 0, diameter, diameter, fill="red")
+  myCanvas.create_line(diameter, 0, 0, diameter, fill="red")
+  myCanvas.pack()
+
+  pos = (frame.winfo_x(), frame.winfo_y())
+  saveBtn = Button(prmpt, text="Save", command=lambda: save(pos))
+
+  frame.pack(pady=20)
+  saveBtn.pack()
+
+def test_f():
+  print(altLoc)
+
+altLoc = "Not set"
+augLoc = "Not set"
+
+mainWindow = setup_main_window("800x500")
+altLocLabel = Label(mainWindow, text=altLoc)
+augLocLabel = Label(mainWindow, text=augLoc)
+altBtn = Button(mainWindow, text="Configure alteration", command=lambda: scope(save_alt))
+augBtn = Button(mainWindow, text="Configure Augmentation", command=lambda: scope(save_aug))
+
+
+
+altBtn.pack()
+altLocLabel.pack()
+
+test = Button(mainWindow, text="test", command=test_f)
+test.pack()
+
+mainWindow.mainloop()
